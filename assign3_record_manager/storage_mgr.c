96608f58c8b48f698ff9fb31ed79e0508052157a
Ramyaa810 <ramyaa810@gmail.com>
2022-10-31 21:50:25 -0500

    commit

--- assign3_record_manager/storage_mgr.c
+++ assign3_record_manager/storage_mgr.c
@@ -118,15 +118,12 @@ RC openPageFile(char *fileName, SM_FileHandle *fHandle)
 
 RC closePageFile (SM_FileHandle *fHandle)
 {
-	//closes the file and returns 0
-	if(fHandle->mgmtInfo != NULL)
-	{
+	// Validation
+	if (fHandle->mgmtInfo == NULL) return RC_FILE_NOT_FOUND;
+
+	// Action
 	if (fclose(fHandle->mgmtInfo) == 0)
 		return RC_OK;
-		else return RC_FILE_NOT_FOUND;
-	}
-	else //case it doesnt exist
-		return RC_FILE_NOT_FOUND;
 }
 
 //destroying the page file
