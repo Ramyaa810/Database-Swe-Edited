abdd0711653ea47ce67dc99c8a6f1117d34ec8b7
Ramyaa810 <ramyaa810@gmail.com>
2022-11-05 21:17:28 -0500

    commit

--- assign3_record_manager/record_mgr.c
+++ assign3_record_manager/record_mgr.c
@@ -383,8 +383,8 @@ RC insertRecord(RM_TableData *rel, Record *record)
 	record->id.page = freepage;
 	record->id.slot = zero;
 	 Schema *schema = rel->schema;
-	char *serializedRecord = callSerializeRecord(record, rel);
-	//char *serializedRecord = serializeRecord(record, schema);
+	//char *serializedRecord = callSerializeRecord(record, rel);
+	char *serializedRecord = serializeRecord(record, schema);
 	BM_BufferPool *bufferPool = ((RecordManager *)rel->mgmtData)->bufferPool;
 	int freepage1 = ((RecordManager *)rel->mgmtData)->freePages[0];
 	pinPage(bufferPool, page, freepage1);
