c33b12d7bd12cb8b51c20d8f74c58f0b8bf5b2ee
Ramyaa810 <ramyaa810@gmail.com>
2022-11-09 00:13:11 -0600

    commit

--- assign3_record_manager/rm_serializer.c
+++ assign3_record_manager/rm_serializer.c
@@ -389,10 +389,10 @@ Schema *deserializeSchema(char *serializedSchemaData)
 	end = createCharObject1();
 	splitchar = createCharObject1();
 
-	start = stringCompare(1,serializedSchemaData);
-	// strtok(serializedSchemaData, "<");
-	end = stringCompare(2,NULL); 
-	//strtok(NULL, ">");
+	//start = stringCompare(1,serializedSchemaData);
+	 start = strtok(serializedSchemaData, "<");
+	//end = stringCompare(2,NULL); 
+	end = strtok(NULL, ">");
 
 	AttrNum = strtol(end, &start, 10);
 
